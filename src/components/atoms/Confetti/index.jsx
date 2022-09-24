744364e34fa4c6fcb66a823eb02102f894af10a1
dominggo1999 <arnolddominggo19@gmail.com>
2022-09-24 11:00:15 +0700

    add-atom : confetti

---
+++ src/components/atoms/Confetti/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import useWindowSize from 'react-use/lib/useWindowSize';
+import useTimeOut from 'react-use/lib/useTimeout';
+import ReactConfetti from 'react-confetti';
+
+const Confetti = ({ duration = 4000 }) => {
+  const { width, height } = useWindowSize();
+  const [isComplete] = useTimeOut(duration);
+
+  return (
+    <ReactConfetti
+      width={width}
+      height={height}
+      recycle={!isComplete()}
+    />
+  );
+};
+
+export default Confetti;
