4b48581b395422349219b2efa574e0c13a053f10
dominggo1999 <arnolddominggo19@gmail.com>
2022-09-24 13:48:42 +0700

    add landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import { BsGithub } from 'react-icons/bs';
+import { IoMdPlay } from 'react-icons/io';
+import Hero from '@/components/organisms/Hero';
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <Hero
+      heroImage="/demo.gif"
+      title="The best quiz app in the world"
+      description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam nulla saepe soluta aliquid qui possimus quisquam vitae odio? Neque, dolor."
+      actions={[{
+        to: 'https://github.com/dominggo1999/quiz',
+        label: 'Source Code',
+        isCallToAction: true,
+        icon: BsGithub,
+        iconPosition: 'left',
+      },
+      {
+        to: '/start-quiz',
+        label: 'Start A Quiz',
+        isCallToAction: true,
+        icon: IoMdPlay,
+        iconPosition: 'left',
+      }]}
+    />
   );
 };
 
